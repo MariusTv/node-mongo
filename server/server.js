5d07befb6a0bc0ac395d90a327b9f8ee77c7f17d
marius-tverijonas <marius@veleza.com>
2018-04-08 19:05:33 +0300

    delete todo

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-const {ObjectId} = require('mongodb');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -35,7 +35,7 @@ app.get('/todos', (req, res) => {
 app.get('/todos/:id', (req, res) => {
     var id = req.params.id;
 
-    if (!ObjectId.isValid(id)) {
+    if (!ObjectID.isValid(id)) {
         return res.status(404).send();
     }
 
@@ -47,6 +47,21 @@ app.get('/todos/:id', (req, res) => {
         res.send({todo});
     }).catch((e) => res.status(400).send());
 
+});
+
+app.delete('/todos/:id', (req, res) => {
+   var id = req.params.id;
+
+   if (!ObjectID.isValid(id)) {
+       return res.status(404).send();
+   }
+
+   Todo.findByIdAndRemove(id).then((todo) => {
+       if (!todo) {
+            return res.status(404).send();
+       }
+       res.send(todo);
+   }).catch((e) => res.status(400).send());
 });
 
 app.listen(port, () => {
