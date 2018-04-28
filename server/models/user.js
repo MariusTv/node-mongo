d85683fe3fa8aef04cd6fdf967261055cd1a52e1
marius-tverijonas <marius@veleza.com>
2018-04-28 17:16:24 +0300

    authenticate middleware

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,76 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-var User = mongoose.model('User', {
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 1
-    }
+var UserSchema = new mongoose.Schema({
+        email: {
+            type: String,
+            required: true,
+            trim: true,
+            minlength: 1,
+            unique: true,
+            validate: {
+                validator: validator.isEmail,
+                message: '{VALUE} is not a valid email'
+            }
+        },
+        password: {
+            type: String,
+            required: true,
+            minlength: 6
+        },
+        tokens: [{        //array of objects (tokens)
+            access: {
+                type: String,
+                required: true
+            },
+            token: {
+                type: String,
+                required: true
+            }
+        }]
 });
+
+//overriding method to return only id and email
+UserSchema.methods.toJSON = function() {
+    var user = this;
+    var userObject = user.toObject();
+
+    return _.pick(userObject, ['_id', 'email'])
+}
+
+//adding out custom method. we do not use arrow function because arrow function do not bind a 'this' keyword
+UserSchema.methods.generateAuthToken = function() {
+    var user = this;
+    var access = 'auth';
+    var token = jwt.sign({_id: user._id.toHexString(), access}, 'blabla_salt').toString();
+
+    user.tokens = user.tokens.concat([{access, token}]);
+
+    return user.save().then(() => {
+        return token;
+    });
+};
+
+//adding static method
+UserSchema.statics.findByToken = function (token) {
+    var User = this;
+    var decoded;
+
+    try {
+        decoded = jwt.verify(token, 'blabla_salt');
+    } catch (e) {
+        return Promise.reject();
+    }
+
+    return User.findOne({
+        _id: decoded._id,
+        'tokens.token': token, //key should be in quoted because of .
+        'tokens.access': 'auth'
+    });
+};
+
+var User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const _ = require('lodash');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT;
@@ -91,6 +92,49 @@ app.patch('/todos/:id', (req, res) => {
 
     }).catch((e) => res.status(400).send());
 })
+
+var authenticate = (req, res, next) => {
+    var token = req.header('x-auth');
+
+    User.findByToken(token).then((user) => {
+        if (!user) {
+            return Promise.reject();
+        }
+
+        req.user = user;
+        req.token = token;
+        next();
+    }).catch((e) => {
+        res.status(401).send();
+    });
+};
+
+//Users
+app.get('/users/me', authenticate ,(req, res) => {
+    var token = req.header('x-auth');
+
+    User.findByToken(token).then((user) => {
+        if (!user) {
+            return Promise.reject();
+        }
+
+        res.send(user);
+    }).catch((e) => {
+        res.status(401).send();
+    });
+});
+
+app.post('/users', (req, res) => {
+    //do not allow users update tokens array
+    var body = _.pick(req.body, ['email', 'password']);
+    var user = new User(body);
+
+    user.save().then(() => {
+        return user.generateAuthToken();
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
+    }).catch((e) => res.status(400).send(e));
+});
 
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
