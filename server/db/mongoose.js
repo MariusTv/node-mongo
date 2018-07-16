486b2aa1ce46727af2a1f3bd13a8f2ee5e2b236f
marius-tverijonas <marius@veleza.com>
2018-07-16 11:19:35 +0300

    use MONGODB_URI on heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,9 +2,7 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise; //tell mongoose that we want to use build in promise library
 
-var mongoDbConnection = process.env.NODE && ~process.env.NODE.indexOf("heroku")
-    ? 'mongodb://marius:h4x1s3@ds137089.mlab.com:37089/node-todo-api' //for heroku you can also use process.env.MONGODB_URI
-    : process.env.MONGODB_URI;
+var mongoDbConnection = process.env.MONGODB_URI;
 
 mongoose.connect(mongoDbConnection);
 
